746203842be2393f1b48be3d68210021e9c38486
g-leiva <76491401+g-leiva@users.noreply.github.com>
2022-10-23 19:58:45 -0600

    cambio en comentarios

--- ACS/Controllers/AccesoController.cs
+++ ACS/Controllers/AccesoController.cs
@@ -11,7 +11,7 @@ using System.Data.SqlClient;
 
 namespace ACS.Controllers
 {
-    // controlador acceso
+    // controlador acceso rama master
     public class AccesoController : ApiController
     {
         [Route("api/autenticar/autenticar")]
